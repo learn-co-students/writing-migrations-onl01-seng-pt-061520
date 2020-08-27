5c0a1b5732a8eb3cc74b9b444d8a6937fc2acbd3
drewsview34 <drewsview34@outlook.com>
2020-08-27 13:14:15 -0700

    Done.

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,7 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+
+    def change
+        change_column :students, :birthdate, :datetime
+        #Ex:- change_column("admin_users", "email", :string, :limit =>25)
+    end
+end
